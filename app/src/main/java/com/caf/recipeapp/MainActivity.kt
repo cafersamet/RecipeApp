4a6139d8cac51d7cdc8e21b783791de751a537b5
Cafer Samet Güllüce <cafersamet25@gmail.com>
2021-06-01 19:53:52 +0300

    gradle files updated for compose

--- app/src/main/java/com/caf/recipeapp/MainActivity.kt
+++ app/src/main/java/com/caf/recipeapp/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.caf.recipeapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MainScreen()
+        }
     }
+}
+@Composable
+fun MainScreen() {
+    Text("Abc")
+}
+
+@Preview
+@Composable
+fun Test() {
+    Text("Abc")
 }
